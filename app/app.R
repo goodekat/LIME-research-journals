290e1bc5984f9dba6e8350889ea0643c0f5117fc
goodekat <kgoode@iastate.edu>
2018-10-25 16:57:26 -0500

    worked on app

--- app/app.R
+++ app/app.R
@@ -15,25 +15,26 @@ library(lime)
 hamby224_test_explain <- readRDS("../data/hamby224_test_explain.rds")
 
 # Create a dataset with all combinations of lands and bullets comparisons for each set
-combs1 <- data.frame(set = factor(1),
+combinations1 <- data.frame(set = factor(1),
                     expand.grid(land1 = factor(1:6),
                                 land2 = factor(1:6),
                                 bullet1 = factor(c("1", "2", "Q")),
                                 bullet2 = factor(c("1", "2", "Q"))))
-combs11 <- data.frame(set = factor(11),
+combinations11 <- data.frame(set = factor(11),
                     expand.grid(land1 = factor(1:6),
                                 land2 = factor(1:6), 
                                 bullet1 = factor(c("1", "2", "I")),
                                 bullet2 = factor(c("1", "2", "I"))))
-combs <- rbind(combs1, combs11)
+combinations <- rbind(combinations1, combinations11)
 
 # Join the combinations and the data so that all combinations have a row in the data
-hamby224_test_explain_NAs <- left_join(combs, hamby224_test_explain,
+hamby224_test_explain_NAs <- left_join(combinations, hamby224_test_explain,
                                        by = c("set", "land1", "land2", "bullet1", "bullet2")) %>%
-  mutate(bullet1 = forcats::fct_recode(bullet1, "bullet 1" = "1", "bullet 2" = "2", 
-                                       "bullet Q" = "Q", "bullet I" = "I"),
-         bullet2 = forcats::fct_recode(bullet2, "bullet 1" = "1", "bullet 2" = "2", 
-                                       "bullet Q" = "Q", "bullet I" = "I"))
+  mutate(bullet1 = forcats::fct_recode(bullet1, "Known 1" = "1", "Known 2" = "2", 
+                                       "Questioned" = "Q", "Questioned" = "I"),
+         bullet2 = forcats::fct_recode(bullet2, "Known 1" = "1", "Known 2" = "2", 
+                                       "Questioned" = "Q", "Questioned" = "I"),
+         rfscore = round(rfscore, 3))
 
 ## ------------------------------------------------------------------------------------
 ## UI setup
@@ -69,20 +70,28 @@ server <- function(input, output) {
     
     # Create a tile plot of the random forest predictions for all land comparisons
     plot <- hamby224_test_explain_NAs %>%
-      filter(set == chosen_set) %>%
+      filter(set == chosen_set,
+             !(bullet2 == "Questioned" & bullet1 == "Known 1"),
+             !(bullet2 == "Questioned" & bullet1 == "Known 2"),
+             !(bullet2 == "Known 2" & bullet1 == "Known 1")) %>%
       select(case, bullet1, bullet2, land1, land2, rfscore) %>%
       distinct() %>%
-      ggplot(aes(x = land1, y = land2, label = bullet1, label2 = bullet2)) +
+      ggplot(aes(x = land1, y = land2, label = bullet1, label2 = bullet2,
+                 text = paste('Random Forest Score: ', ifelse(is.na(rfscore), "Missing due to tank rash", rfscore),
+                              '\nBullets Compared: ', bullet2, "- Land ", land1, "vs", bullet1, "- Land", land2))) +
       geom_tile(aes(fill = rfscore)) +
       facet_grid(bullet1 ~ bullet2, scales = "free") +
-      theme_bw() +
-      scale_fill_gradient2(low = "grey", high = "orange", midpoint = 0.5) +
+      theme_minimal() +
+      scale_fill_gradient2(low = "darkgrey", high = "darkorange", midpoint = 0.5) +
       labs(x = "Land 1", y = "Land 2", fill = "RF Score", 
            title = paste0("Hamby 224 Data (", input$testset, ")"))
     
-    ggplotly(plot, source = "tileplot", width = 700, height = 550)
+    ggplotly(plot, source = "tileplot", width = 700, height = 550, tooltip = "text")
     
   })
+  
+  # Obtain the xlimits
+  xlimit <- max(abs(hamby224_test_explain_NAs$feature_weight), na.rm = TRUE)
   
   # Create my own feature plot
   output$featureplot <- renderPlot({
@@ -96,14 +105,12 @@ server <- function(input, output) {
         
         # Obtain the number of the chosen test set
         chosen_set = unlist(strsplit(input$testset, split = " "))[2]
-        
+      
         # Create a dataset with the connection between the curveNumbers and the bullet facets
-        bullet_locations <- data.frame(set = rep(c("1", "11"), each = 9),
-                                       curveNumber = rep(0:8, 2),
-                                       bullet1 = c(rep(c("bullet 1", "bullet 2", "bullet Q"), 3), 
-                                                   rep(c("bullet 1", "bullet 2", "bullet I"), 3)),
-                                       bullet2 = c(rep(c("bullet 1", "bullet 2", "bullet Q"), each = 3), 
-                                                   rep(c("bullet 1", "bullet 2", "bullet I"), each = 3)))
+        bullet_locations <- data.frame(set = rep(c("1", "11"), each = 6),
+                                       curveNumber = rep(0:5, 2),
+                                       bullet1 = c("Known 1", "Known 1", "Known 2", "Known 1", "Known 2", "Questioned"),
+                                       bullet2 = c("Known 1", "Known 2", "Known 2", "Questioned", "Questioned", "Questioned"))
         
         # Create a dataset with the location of cell that was clicked
         location <- data.frame(land1 = click_data$x, 
@@ -119,30 +126,29 @@ server <- function(input, output) {
                  land2 == location$land2,
                  bullet1 == location$bullet1,
                  bullet2 == location$bullet2) %>%
-          slice(1:3) %>%
-          mutate(feature_desc = reorder(feature_desc, as.numeric(feature_weight)),
+          mutate(feature_desc = reorder(feature_desc, abs(as.numeric(feature_weight))),
                  evidence = factor(if_else(feature_weight >= 0, "Supports", "Contradicts"), 
                                    levels = c("Contradicts", "Supports")))
         
+        # Create a data frame with the appropriate labels
         labels <- selected_comparison %>% 
           slice(1) %>%
-          select(study, set, bullet1, bullet2, land1, land2)
+          select(study, set, bullet1, bullet2, land1, land2, rfscore, samesource)
         
+        # Create the plot
         ggplot(selected_comparison, aes(x = feature_desc, y = feature_weight)) + 
-          geom_col(aes(fill = evidence)) + 
+          geom_col(aes(fill = evidence)) +
+          ylim(-xlimit, xlimit) +
           coord_flip() + 
           theme_minimal() +
           theme(legend.position = "bottom") +
           labs(y = "Feature Weight", x = "Feature Bin", fill = "Evidence:",
                title = "Top Three Features Chosen by LIME for the Selected Comparison",
-               subtitle = paste0("Selected comparison information: \n", 
-                                 "   Study: ", labels$study, "\n",
-                                 "   Set: ", labels$set, "\n",
-                                 "   1st Bullet: ", labels$bullet1, "\n",
-                                 "   2nd Bullet: ", labels$bullet2, "\n",
-                                 "   First Land: ", labels$land1, "\n",
-                                 "   Second Land: ", labels$land2, "\n")) + 
-          scale_fill_manual(values = c("Supports" = "#00BFC4", "Contradicts" = "#F8766D"),
+               subtitle = paste0("Study: ", labels$study, " Set ", labels$set, "\n",
+                                 "Bullets Compared: ", labels$bullet1, " - Land ", labels$land1, " vs ", labels$bullet2, " - Land ", labels$land2, "\n",
+                                 "Random Forest Score: ", round(labels$rfscore, 3), "\n",
+                                 "Predicted Match: ", labels$samesource)) + 
+          scale_fill_manual(values = c("Supports" = "darkorange", "Contradicts" = "darkgrey"),
                             drop = FALSE)
         
       } else{
