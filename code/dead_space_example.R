6ba01c2d891cba82b3344dbd0ee1ed9fac2f5519
goodekat <kgoode@iastate.edu>
2018-10-30 10:39:49 -0500

    added a dead space example

---
+++ code/dead_space_example.R
@@ -0,0 +1,34 @@
+# Example with Plotly to show dead space in heatmap
+
+# System information
+# R version 3.5.1 (2018-07-02)
+# Platform: x86_64-apple-darwin15.6.0 (64-bit)
+# Running under: macOS  10.14
+
+# Load libraries
+library(plotly) # version ‘4.8.0’
+library(ggplot2) # version ‘3.1.0’
+library(tidyr) # version ‘0.8.2’
+
+# Example from: https://plot.ly/r/heatmaps/ -------------------------------------------
+
+# Create dataset
+m <- matrix(rnorm(9), nrow = 3, ncol = 3)
+
+# Create plotly heatmap - no deadspace to be found
+plot_ly(x = c("a", "b", "c"), y = c("d", "e", "f"), z = m, type = "heatmap")
+
+# Example using ggplotly function -----------------------------------------------------
+
+# Reshape the data for ggplot
+m_gathered <- data.frame(m) %>%
+  gather(key = column) %>%
+  mutate(row = factor(rep(c("X1", "X2", "X3"), 3))) %>%
+  select(column, row, value)
+
+# Create ggplot heatmap
+p <- ggplot(m_gathered, aes(x = column, y = row, fill = value)) +
+  geom_tile()
+
+# Apply plotly to ggplot heatmap - deadspace in the middle of (X1, X1)
+ggplotly(p)
---
+++ code/individual_plots.R
@@ -0,0 +1,100 @@
+library(plotly)
+library(ggplot)
+
+# Input data
+hamby224_test_explain <- readRDS("./data/hamby224_test_explain.rds")
+
+# Create a dataset with all combinations of lands and bullets comparisons for each set
+combinations1 <- data.frame(set = factor(1),
+                            expand.grid(land1 = factor(1:6),
+                                        land2 = factor(1:6),
+                                        bullet1 = factor(c("1", "2", "Q")),
+                                        bullet2 = factor(c("1", "2", "Q"))))
+combinations11 <- data.frame(set = factor(11),
+                             expand.grid(land1 = factor(1:6),
+                                         land2 = factor(1:6), 
+                                         bullet1 = factor(c("1", "2", "I")),
+                                         bullet2 = factor(c("1", "2", "I"))))
+combinations <- rbind(combinations1, combinations11)
+
+# Join the combinations and the data so that all combinations have a row in the data
+hamby224_test_explain_NAs <- left_join(combinations, hamby224_test_explain,
+                                       by = c("set", "land1", "land2", "bullet1", "bullet2")) %>%
+  mutate(bullet1 = forcats::fct_recode(bullet1, "Known 1" = "1", "Known 2" = "2", 
+                                       "Questioned" = "Q", "Questioned" = "I"),
+         bullet2 = forcats::fct_recode(bullet2, "Known 1" = "1", "Known 2" = "2", 
+                                       "Questioned" = "Q", "Questioned" = "I"))
+
+p1 <- hamby224_test_explain_NAs %>%
+  filter(set == chosen_set, bullet1 == "Known 1", bullet2 == "Known 1") %>%
+  select(case, bullet1, bullet2, land1, land2, rfscore) %>%
+  distinct() %>%
+  ggplot(aes(x = land1, y = land2, label = bullet1, label2 = bullet2)) +
+  geom_tile(aes(fill = rfscore)) +
+  facet_grid(bullet1 ~ bullet2, scales = "free") +
+  theme_bw() +
+  scale_fill_gradient2(low = "grey", high = "orange", midpoint = 0.5) +
+  labs(x = "Land 1", y = "Land 2", fill = "RF Score")
+
+p2 <- hamby224_test_explain_NAs %>%
+  filter(set == chosen_set, bullet1 == "Known 1", bullet2 == "Known 2") %>%
+  select(case, bullet1, bullet2, land1, land2, rfscore) %>%
+  distinct() %>%
+  ggplot(aes(x = land1, y = land2, label = bullet1, label2 = bullet2)) +
+  geom_tile(aes(fill = rfscore)) +
+  facet_grid(bullet1 ~ bullet2, scales = "free") +
+  theme_bw() +
+  scale_fill_gradient2(low = "grey", high = "orange", midpoint = 0.5) +
+  labs(x = "Land 1", y = "Land 2", fill = "RF Score")
+
+p3 <- hamby224_test_explain_NAs %>%
+  filter(set == chosen_set, bullet1 == "Known 1", bullet2 == "Questioned") %>%
+  select(case, bullet1, bullet2, land1, land2, rfscore) %>%
+  distinct() %>%
+  ggplot(aes(x = land1, y = land2, label = bullet1, label2 = bullet2)) +
+  geom_tile(aes(fill = rfscore)) +
+  facet_grid(bullet1 ~ bullet2, scales = "free") +
+  theme_bw() +
+  scale_fill_gradient2(low = "grey", high = "orange", midpoint = 0.5) +
+  labs(x = "Land 1", y = "Land 2", fill = "RF Score")
+
+p4 <- ggplot() + geom_blank() + theme_classic()
+
+p5 <- hamby224_test_explain_NAs %>%
+  filter(set == chosen_set, bullet1 == "Known 2", bullet2 == "Known 2") %>%
+  select(case, bullet1, bullet2, land1, land2, rfscore) %>%
+  distinct() %>%
+  ggplot(aes(x = land1, y = land2, label = bullet1, label2 = bullet2)) +
+  geom_tile(aes(fill = rfscore)) +
+  facet_grid(bullet1 ~ bullet2, scales = "free") +
+  theme_bw() +
+  scale_fill_gradient2(low = "grey", high = "orange", midpoint = 0.5) +
+  labs(x = "Land 1", y = "Land 2", fill = "RF Score")
+
+p6 <- hamby224_test_explain_NAs %>%
+  filter(set == chosen_set, bullet1 == "Known 2", bullet2 == "Questioned") %>%
+  select(case, bullet1, bullet2, land1, land2, rfscore) %>%
+  distinct() %>%
+  ggplot(aes(x = land1, y = land2, label = bullet1, label2 = bullet2)) +
+  geom_tile(aes(fill = rfscore)) +
+  facet_grid(bullet1 ~ bullet2, scales = "free") +
+  theme_bw() +
+  scale_fill_gradient2(low = "grey", high = "orange", midpoint = 0.5) +
+  labs(x = "Land 1", y = "Land 2", fill = "RF Score")
+
+p7 <- ggplot() + geom_blank() + theme_classic()
+
+p8 <- ggplot() + geom_blank() + theme_classic()
+
+p9 <- hamby224_test_explain_NAs %>%
+  filter(set == chosen_set, bullet1 == "Questioned", bullet2 == "Questioned") %>%
+  select(case, bullet1, bullet2, land1, land2, rfscore) %>%
+  distinct() %>%
+  ggplot(aes(x = land1, y = land2, label = bullet1, label2 = bullet2)) +
+  geom_tile(aes(fill = rfscore)) +
+  facet_grid(bullet1 ~ bullet2, scales = "free") +
+  theme_bw() +
+  scale_fill_gradient2(low = "grey", high = "orange", midpoint = 0.5) +
+  labs(x = "Land 1", y = "Land 2", fill = "RF Score")
+
+subplot(p1, p2, p3, p4, p5, p6, p7, p8, p9, nrows = 3, margin = 0.03)
