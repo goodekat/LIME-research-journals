d3a5c4631c279f7b5c222577728d235e4985f064
goodekat <kgoode@iastate.edu>
2019-01-25 17:36:59 -0600

    worked on cleaning up code after rfscore has been added

--- code/helper_functions.R
+++ code/helper_functions.R
@@ -13,12 +13,20 @@ library(tree)
 
 treebink <- function (y, x, k, minsize = 10) {
   
+  # Set the mindev: "The within-node deviance must be at least this times 
+  # that of the root node for the node to be split." 0.01 is the default
+  # value in the tree package.
   dev <- 0.01
-  tree <- tree::tree(y ~ x, control = tree::tree.control(nobs = length(y), mindev=dev, minsize = minsize))
+  
+  # Fit a tree with starting specifications
+  tree <- tree::tree(y ~ x, control = tree::tree.control(nobs = length(y), 
+                                                         mindev = dev, 
+                                                         minsize = minsize))
   while ((length(grep("<leaf>", tree$frame$var)) < k) & (dev > 1e-9)) {
     dev <- dev/10
     tree <- tree::tree(y ~ x, control = tree::tree.control(nobs = length(y), mindev=dev,  minsize = minsize))
   }
+  
   tree <- tree::prune.tree(tree, best = k)
   breaks <- na.omit(unique(parse_number(tree$frame$splits[,1])))
   if (length(breaks) == k-1) return(sort(breaks)) # and we're happy because we got all the values we need
@@ -39,7 +47,6 @@ treebink <- function (y, x, k, minsize = 10) {
   breaks
   
 }
-
 
 # Examples:
 # treebink(car.test.frame$Mileage, car.test.frame$Weight, k = 5)
